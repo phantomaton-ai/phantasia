00dc506302d352e8a404de5adf2b96e3541d77dc
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-04-16 22:05:04 +0000

    cli

---
+++ cli.js
@@ -0,0 +1,38 @@
+import phantasia from './phantasia.js';
+
+const filename = process.argv[2];
+
+if (!filename || filename.length < 1) {
+  console.log('Usage: phantasia <filename>');
+  process.exit();
+}
+
+console.log(filename);
+
+const input = await new Promise(resolve => {
+  let data = '';
+
+  if (process.stdin.isTTY) {
+    process.stdin.on('data', (data) => {
+      const line = data.toString().trim();
+      if (line.endsWith('\\')) {
+        data += line.slice(0, line.length - 1) + '\n';
+      } else {
+        resolve(data + line);
+      }
+    });
+  } else {
+    process.stdin.on('readable', () => {
+      let chunk;
+      while ((chunk = process.stdin.read()) !== null) {
+        data += chunk;
+      }
+    });
+    
+    process.stdin.on('end', () => {
+      resolve(data);
+    });
+  }  
+});
+
+console.log("INPUT", input);
